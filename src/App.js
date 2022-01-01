c30abd4121f6f48edd9d8ab264c3c776af52d996
Lucas Argerich <lucasargerichcoder@gmail.com>
2022-01-01 19:53:08 -0300

    Proyect Setup & File Structure

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './app.scss'
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="websiteTitle">Photo Gallery</h1>
+      
+    </div>
+  );
+}
+
+export default App;
